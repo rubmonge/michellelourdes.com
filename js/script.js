55efc6f43b7e5741922c22ab8814fd4b1bb0ef71
rubmonge <rubenmonge@gmail.com>
2021-12-27 18:30:26 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+  $(function(){
+	// Initialize Masonry
+	$('#content').masonry({
+		columnWidth: 320,
+		itemSelector: '.item',
+		isFitWidth: true,
+		isAnimated: !Modernizr.csstransitions
+	}).imagesLoaded(function() {
+		$(this).masonry('reload');
+	});
+
+});
